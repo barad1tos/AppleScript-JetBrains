294c66135414421eb209be6e21566e3c71a913d8
Roman Borodavkin <barad1tos@gmail.com>
2026-06-02 15:05:39 +0300

    feat(highlighting): add richer AppleScript token colors
    - Add dedicated syntax highlight keys for logical operators, comparison operators, language literals, and built-in types
    
    - Register the new color settings descriptors and preview sample without deprecated color APIs
    
    - Add a regression test that freezes the token-to-highlight-key mapping
    
    Impact: AppleScript files get more readable syntax coloring before dictionary-backed semantic highlighting runs

--- src/main/kotlin/com/intellij/plugin/applescript/lang/ide/highlighting/AppleScriptColorsAndFontsPage.kt
+++ src/main/kotlin/com/intellij/plugin/applescript/lang/ide/highlighting/AppleScriptColorsAndFontsPage.kt
@@ -10,7 +10,7 @@ import javax.swing.Icon
 
 class AppleScriptColorsAndFontsPage : ColorSettingsPage {
 
-    override fun getIcon(): Icon? = AppleScriptFileType.icon
+    override fun getIcon(): Icon = AppleScriptFileType.icon
 
     override fun getHighlighter(): SyntaxHighlighter = AppleScriptSyntaxHighlighter()
 
@@ -27,11 +27,18 @@ class AppleScriptColorsAndFontsPage : ColorSettingsPage {
     private companion object {
         private val ATTRS: Array<AttributesDescriptor> = arrayOf(
             AttributesDescriptor("Keyword", AppleScriptSyntaxHighlighterColors.KEYWORD),
+            AttributesDescriptor("Logical operator", AppleScriptSyntaxHighlighterColors.LOGICAL_OPERATOR),
+            AttributesDescriptor("Comparison operator", AppleScriptSyntaxHighlighterColors.COMPARISON_OPERATOR),
+            AttributesDescriptor("Language literal", AppleScriptSyntaxHighlighterColors.LANGUAGE_LITERAL),
+            AttributesDescriptor("Built-in type", AppleScriptSyntaxHighlighterColors.BUILT_IN_TYPE),
             AttributesDescriptor("String", AppleScriptSyntaxHighlighterColors.STRING),
             AttributesDescriptor("Operator", AppleScriptSyntaxHighlighterColors.OPERATION_SIGN),
             AttributesDescriptor("Comment", AppleScriptSyntaxHighlighterColors.COMMENT),
             AttributesDescriptor("Dictionary command", AppleScriptSyntaxHighlighterColors.DICTIONARY_COMMAND_ATTR),
-            AttributesDescriptor("Command parameter name", AppleScriptSyntaxHighlighterColors.DICTIONARY_COMMAND_SELECTOR_ATTR),
+            AttributesDescriptor(
+                "Command parameter name",
+                AppleScriptSyntaxHighlighterColors.DICTIONARY_COMMAND_SELECTOR_ATTR,
+            ),
             AttributesDescriptor("Dictionary class", AppleScriptSyntaxHighlighterColors.DICTIONARY_CLASS_ATTR),
             AttributesDescriptor("Dictionary property", AppleScriptSyntaxHighlighterColors.DICTIONARY_PROPERTY_ATTR),
             AttributesDescriptor("Dictionary constant", AppleScriptSyntaxHighlighterColors.DICTIONARY_CONSTANT_ATTR),
@@ -39,6 +46,10 @@ class AppleScriptColorsAndFontsPage : ColorSettingsPage {
 
         private val OUR_TAGS: Map<String, TextAttributesKey> = mapOf(
             "keyword" to AppleScriptSyntaxHighlighterColors.KEYWORD,
+            "logical operator" to AppleScriptSyntaxHighlighterColors.LOGICAL_OPERATOR,
+            "comparison operator" to AppleScriptSyntaxHighlighterColors.COMPARISON_OPERATOR,
+            "language literal" to AppleScriptSyntaxHighlighterColors.LANGUAGE_LITERAL,
+            "built-in type" to AppleScriptSyntaxHighlighterColors.BUILT_IN_TYPE,
             "string" to AppleScriptSyntaxHighlighterColors.STRING,
             "operator" to AppleScriptSyntaxHighlighterColors.OPERATION_SIGN,
             "comment" to AppleScriptSyntaxHighlighterColors.COMMENT,
@@ -49,23 +60,33 @@ class AppleScriptColorsAndFontsPage : ColorSettingsPage {
             "dictionary constant" to AppleScriptSyntaxHighlighterColors.DICTIONARY_CONSTANT_ATTR,
         )
 
-        private val DEMO_TEXT: String = """searchFiles for ff of minimumValue(2, 3) for "LeChateau"
-text from word (copy 5 to c) to word 4 of "We're all in this together"
-minimumValue(2, maximumValue(x, y))
-if file_type comes before "APPL" then copy 5 to c
-if reportsToPrint > 0 then
-    tell application "ReportWizard"
-    -- Statements to print the reports.
-    end tell
-end if ## had some reports to print
-tell application "TextEdit"
-  <command>make</command> <command parameter>new</command parameter> <dictionary class>document</dictionary class> <command parameter>with properties</command parameter> {name:"Doc Name", text:"Now I can create new documents ¬
-using AppleScript."}
-  get <dictionary property>message id</dictionary property> of it's <dictionary class>mailbox</dictionary class>
-  <command>close</command>
-  get <dictionary class>character</dictionary class>
-  set pathToSpec to <dictionary constant>application support</dictionary constant>
-  set character to 1234
-end tell"""
+        private const val DEMO_TEXT: String =
+            "searchFiles for ff of minimumValue(2, 3) for \"LeChateau\"\n" +
+                "text from word (copy 5 to c) to word 4 of \"We're all in this together\"\n" +
+                "minimumValue(2, maximumValue(x, y))\n" +
+                "if file_type comes before \"APPL\" then copy 5 to c\n" +
+                "if reportsToPrint > 0 then\n" +
+                "    tell application \"ReportWizard\"\n" +
+                "    -- Statements to print the reports.\n" +
+                "    end tell\n" +
+                "end if ## had some reports to print\n" +
+                "if track_count <comparison operator>></comparison operator> 0 " +
+                "<logical operator>and</logical operator> statusText is not " +
+                "<language literal>missing value</language literal> then\n" +
+                "  set raw_year to raw_year as <built-in type>integer</built-in type>\n" +
+                "end if\n" +
+                "tell application \"TextEdit\"\n" +
+                "  <command>make</command> <command parameter>new</command parameter> " +
+                "<dictionary class>document</dictionary class> " +
+                "<command parameter>with properties</command parameter> " +
+                "{name:\"Doc Name\", text:\"Now I can create new documents ¬\n" +
+                "using AppleScript.\"}\n" +
+                "  get <dictionary property>message id</dictionary property> of it's " +
+                "<dictionary class>mailbox</dictionary class>\n" +
+                "  <command>close</command>\n" +
+                "  get <dictionary class>character</dictionary class>\n" +
+                "  set pathToSpec to <dictionary constant>application support</dictionary constant>\n" +
+                "  set character to 1234\n" +
+                "end tell"
     }
 }
